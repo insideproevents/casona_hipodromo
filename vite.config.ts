0e3cd1c4f2de67d2d78d3d201dcf99533d253fb2
Ignacio <insideproevents@gmail.com>
2026-03-18 15:57:39 -0300

    Fix paths for images

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/casona_hipodromo/',
+  base: './',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
