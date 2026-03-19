c3e9c339cd42c51dbdcbddf258a2ae35909552d5
Ignacio <insideproevents@gmail.com>
2026-03-19 19:37:29 -0300

    Increase lightbox z-index to appear above dialog

--- src/sections/LocalesSection.tsx
+++ src/sections/LocalesSection.tsx
@@ -387,7 +387,7 @@ export default function LocalesSection() {
       {/* Lightbox */}
       {selectedImage && (
         <div 
-          className="fixed inset-0 z-50 bg-black/95 flex items-center justify-center"
+          className="fixed inset-0 z-[100] bg-black/95 flex items-center justify-center"
           onClick={closeLightbox}
         >
           <button
